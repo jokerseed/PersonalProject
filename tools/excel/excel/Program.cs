6bd2cabcb12b630a25258378079a3bc4f419562e
jokerseed <573202560@qq.com>
2019-07-31 00:19:00 +0800

    。

---
+++ tools/excel/excel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("test" + "\t" + "test1");
+            //Console.WriteLine("test2");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tools/excel/excel/util/ScriptBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel.util
+{
+    class ScriptBuilder
+    {
+        private StringBuilder _builder = new StringBuilder();
+        private StringBuilder _blank = new StringBuilder();
+
+        //添加一行
+        public void AppendLine(string message)
+        {
+            _builder.Append(_blank);
+            _builder.AppendLine(message);
+        }
+
+        //     \t == tab
+        //添加一个tab
+        private void BeginBlank()
+        {
+            _blank.Append('\t');
+        }
+
+        private void EndBlank()
+        {
+            _blank.Remove(_blank.Length - 1, 1);
+        }
+
+        private void BeginNamespace(string name)
+        {
+            BeginBrace($"namespace {name}");
+        }
+
+        private void EndNamespace()
+        {
+            EndBrace();
+        }
+
+        private void BeginBrace(string code)
+        {
+            AppendLine($"{code}");
+            AppendLine("{");
+            BeginBlank();
+        }
+
+        private void EndBrace()
+        {
+            EndBlank();
+            AppendLine("}");
+        }
+    }
+}
