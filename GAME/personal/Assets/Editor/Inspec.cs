7d8590e473d5105b73c4235fa13fa8dbeca7c770
573202560@qq.com <LIUquan123>
2020-09-15 23:03:46 +0800

    。

---
+++ GAME/personal/Assets/Editor/Inspec.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/*
+ * 窗口编辑器
+ */
+[CustomEditor(typeof(Example2))]
+public class Inspec : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GUILayout.BeginVertical(EditorStyles.helpBox);
+        GUILayout.Label("example2");
+        if (GUILayout.Button("xxx")) {
+            Debug.Log("hello world");
+        }
+        GUILayout.EndVertical();
+    }
+}
---
+++ GAME/personal/Assets/Scripts/AnimTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimTest : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_Animator;
+
+    private AudioSource m_Source;
+
+    private Button btn;
+    private ScrollRect sr;
+    
+
+    private void Start()
+    {
+        m_Source = gameObject.AddComponent<AudioSource>();
+
+        var clip  =AssetDatabase.LoadAssetAtPath<AudioClip>("Assets/Audio/scavengers_music.aif");
+        m_Source.clip = clip;
+        m_Source.Play();
+    }
+
+
+    private void OnGUI()
+    {
+
+        if (GUILayout.Button("PlayAnim"))
+        {
+            m_Animator.SetTrigger("myAnim");
+        }
+
+        if (GUILayout.Button("PlayAnim2"))
+        {
+            m_Animator.Play("Img");
+        }
+
+        if (GUILayout.Button("PlayAnim3"))
+        {
+            m_Animator.Play(Animator.StringToHash("Img"));
+        }
+
+        if (GUILayout.Button("PlayAnim4"))
+        {
+            m_Animator.Play(Animator.StringToHash("Img"), -1, 0.5f);
+        }
+    }
+}
