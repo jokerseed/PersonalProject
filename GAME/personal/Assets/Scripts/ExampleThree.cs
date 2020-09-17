3eb622074361c6db547c5011c46e2d5b92f431c9
573202560@qq.com <LIUquan123>
2020-09-17 20:26:17 +0800

    。

--- GAME/personal/Assets/Scripts/ExampleThree.cs
+++ GAME/personal/Assets/Scripts/ExampleThree.cs
@@ -15,6 +15,8 @@ public class ExampleThree : MonoBehaviour
 
     private GameObject db;
 
+    public event Action OnUpdate;
+    
     private void Awake()
     {
 
@@ -36,7 +38,7 @@ public class ExampleThree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //OnUpdate?.Invoke();
     }
 
     void clickHandle()
@@ -59,17 +61,19 @@ class dragCom : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("开始拖动");
+        //Debug.Log("开始拖动");
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("拖动中");
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(gameObject.GetRectTransform(), eventData.position, eventData.pressEventCamera, out v))
-        {
-            gameObject.GetRectTransform().anchoredPosition = v;
-        }
-        Debug.Log(v.ToString());
+        gameObject.GetRectTransform().anchoredPosition += eventData.delta;
+
+        //Debug.Log("拖动中");
+        //if (RectTransformUtility.ScreenPointToLocalPointInRectangle(gameObject.GetRectTransform(), eventData.position, eventData.pressEventCamera, out v))
+        //{
+        //    gameObject.GetRectTransform().anchoredPosition = v;
+        //}
+        //Debug.Log(v.ToString());
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -91,64 +95,7 @@ public class GUIDefaultControls
     }
 }
 
-/*
- * 双击按钮
- */
-public class DoubleButton : Button
-{
-    [Serializable]
-    public class DoubleClickedEvent : UnityEvent { }
-    [SerializeField]
-    private DoubleClickedEvent m_onDoubleClick = new DoubleClickedEvent();
 
-    //这个是双击成功后激活的事件
-    public DoubleClickedEvent onDoubleClick
-    {
-        get { return m_onDoubleClick; }
-        set { m_onDoubleClick = value; }
-    }
-    private DateTime m_firstTime;
-    private DateTime m_secondTime;
-    private void Press()
-    {
-        if (null != onDoubleClick)
-            onDoubleClick.Invoke();
-        resetTime();
-    }
-    public override void OnPointerDown(PointerEventData eventData)
-    {
-        base.OnPointerDown(eventData);
-        // 按下按钮时对两次的时间进行记录
-        if (m_firstTime.Equals(default(DateTime)))
-            m_firstTime = DateTime.Now;
-        else
-            m_secondTime = DateTime.Now;
-    }
-    public override void OnPointerUp(PointerEventData eventData)
-    {
-        base.OnPointerUp(eventData);
-        // 在第二次鼠标抬起的时候进行时间的触发,时差小于400ms触发
-        if (!m_firstTime.Equals(default(DateTime)) && !m_secondTime.Equals(default(DateTime)))
-        {
-            var intervalTime = m_secondTime - m_firstTime;
-            float milliSeconds = intervalTime.Seconds * 1000 + intervalTime.Milliseconds;
-            if (milliSeconds < 400)
-                Press();
-            else
-                resetTime();
-        }
-    }
-    public override void OnPointerExit(PointerEventData eventData)
-    {
-        base.OnPointerExit(eventData);
-        resetTime();
-    }
-    private void resetTime()
-    {
-        m_firstTime = default(DateTime);
-        m_secondTime = default(DateTime);
-    }
-}
 
 /*
  * 长按按钮
---
+++ GAME/personal/Assets/Scripts/PageHome.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class PageHome : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Go1;
+    [SerializeField]
+    private GameObject m_Go2;
+    [SerializeField]
+    private GameObject m_Go3;
+    [SerializeField]
+    private GameObject m_Go4;
+    [SerializeField]
+    private GameObject m_Go5;
+    [SerializeField]
+    private GameObject m_Go6;
+
+    public ExampleThree THREE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        THREE.OnUpdate += OnMainUpdate; //() => {};
+        THREE.OnUpdate += OnMainUpdate2; //() => {};
+    }
+
+    private void OnMainUpdate()
+    {
+        Debug.Log("tHREE MAIN");
+    }
+
+    private void OnMainUpdate2()
+    {
+        Debug.Log("tHREE MAIN222222");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        THREE.OnUpdate -= OnMainUpdate;
+        THREE.OnUpdate -= OnMainUpdate2;
+    }
+}
