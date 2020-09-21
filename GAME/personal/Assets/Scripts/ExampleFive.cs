6fd60de1ce5f2cc41b0e6858cf8d0114a1991bd3
573202560@qq.com <LIUquan123>
2020-09-21 22:05:03 +0800

    。

---
+++ GAME/personal/Assets/Scripts/ExampleFive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleFive : MonoBehaviour
+{
+    public GameObject gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 v1 = Vector2.one;
+        Vector2 v2 = Vector2.one;
+        var cos = Vector2.Dot(v1.normalized, v2.normalized);
+        Debug.Log(cos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
